000c059fd28f775e83460f12ccbab32067e9fc75
Arwa-Hesham <aa2301515@tkh.edu.eg>
2025-11-15 12:44:50 +0200

    lcd shield buttons

---
+++ HMI/Button.h
@@ -0,0 +1,15 @@
+#include "macros_types.h"
+#include <avr/io.h>
+#include "ADC.h"
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+#define ButtonChannel               0
+
+
+/* Functions Prototypes */
+char ButtonSelectVal(void);
+
+#endif
+// BUTTON_H_
